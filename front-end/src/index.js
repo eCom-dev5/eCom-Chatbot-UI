9c450b81682e6a10752fe313435eeaf4764fb6c3
Henil Gajjar <henilgajjar08@gmail.com>
2024-11-16 23:05:38 -0500

    update port

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routing";
+const port = process.env.PORT || 8080;
 
 import "./index.css";
 
