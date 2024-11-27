837ed1c04041ef7f8e1549d272cd4a41eb1c5049
Nishith-Reddy <nishith.ipad@gmail.com>
2024-11-27 15:47:26 -0500

    updated urls

--- back-end/index.js
+++ back-end/index.js
@@ -65,7 +65,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 api.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://verta-frontend-7k7ldne36a-ue.a.run.app");
+  res.header("Access-Control-Allow-Origin", "https://verta-frontend-403080441770.us-east1.run.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
