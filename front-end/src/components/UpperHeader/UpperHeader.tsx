453a8472bfbd27e79594a9c9358c297911f41b34
Fatima Jawadwala <fatemajawadwala8@gmail.com>
2024-12-08 22:53:36 -0500

    changes made in the upperheader

--- front-end/src/components/UpperHeader/UpperHeader.tsx
+++ front-end/src/components/UpperHeader/UpperHeader.tsx
@@ -3,14 +3,24 @@ import { Link } from "react-router-dom";
 import MainNav from "../MainNav/MainNav";
 import styles from "./UpperHeader.module.css";
 
-
 export default function UpperHeader() {
   return (
     <div className={styles.header}>
       <div className={styles.logoContainer}>
-        <Link to="/" className={styles.logo}>Pixel<span className={styles.orange}> Pop</span></Link>
+        <Link to="/" className={styles.logo}>
+          Pixel<span className={styles.orange}> Pop</span>
+        </Link>
       </div>
-      <MainNav />
+      <div className={styles.actions}>
+        <Link to="/login" className={styles.login}>
+          Log In
+        </Link>
+        <Link to="/register" className={styles.register}>
+          Register
+        </Link>
+      </div>
     </div>
   );
 }
+
+
